82bfc2650201837bd8fcafafd9c1fb6986ffee7f
Jeff G <jeff.glaister@gmail.com>
2021-11-08 15:36:28 -0500

    Debug

--- estimate_t2star/workflows.py
+++ estimate_t2star/workflows.py
@@ -111,7 +111,7 @@ def create_mtr_workflow(scan_directory: str, patient_id: str = None, scan_id: st
     #split_mton_flag = False
     #print(split_mton_flag)
 
-    mtfile_node = pe.Node(util.IdentityInterface(fields=['mton_file', 'mtoff_file']), name='mtfile_node')
+    mtfile_node = pe.Node(util.IdentityInterface(fields=['mton_file', 'mtoff_file'], mandatory_inputs=False), name='mtfile_node')
 
     #if split_mton_flag:
     #    split_mt = pe.Node(fsl.Split(), 'split_mt')
